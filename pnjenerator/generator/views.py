213954ccf142a44e81b2252c64c3c361902db40a
JORDAN Kévin <jordan.kevin57@gmail.com>
2022-04-01 15:37:46 +0200

    Use template

--- pnjenerator/generator/views.py
+++ pnjenerator/generator/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.translation import activate
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    activate('fr')
+    return render(request, 'generator/index.html')
